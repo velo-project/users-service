74c7c19197e7f59c25b77f5a88893772be5a0672
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-24 00:32:45 -0300

    feat(ports): implement attributes in new user persistence ports

--- src/main/java/com/github/veloproject/userservices/core/use_cases/register_new_user/implementations/ports/NewUserPersistencePort.java
+++ src/main/java/com/github/veloproject/userservices/core/use_cases/register_new_user/implementations/ports/NewUserPersistencePort.java
@@ -1,17 +1,27 @@
 package com.github.veloproject.userservices.core.use_cases.register_new_user.implementations.ports;
 
 import com.github.veloproject.userservices.core.use_cases.register_new_user.io.inputs.RegisterNewUserUseCaseInput;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.util.Optional;
 
 public interface NewUserPersistencePort {
     Optional<NewUserPersistancePortOutput> execute(RegisterNewUserUseCaseInput input);
 
-    class NewUserPersistancePortOutput {
-
-    }
-
+    @Getter
+    @Builder
     class NewUserPersistancePortInput {
-
+        private String fullName;
+        private String preferedName;
+        private String email;
+        private String password;
     }
+
+    @Getter
+    @Builder
+    class NewUserPersistancePortOutput {
+        private Long userId;
+    }
+
 }
