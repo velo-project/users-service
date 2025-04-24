2197395f7b28c9eeb373f96c64c2ddd190fe6461
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-24 00:33:16 -0300

    test: add test file

---
+++ src/test/java/com/github/veloproject/userservices/UserEntityServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.veloproject.userservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEntityServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
