adf9fd9446d12e4345d133996b88c32176144bbd
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-28 02:50:53 -0300

    refactor: align variable type with user ID type in RegisterNewUserCommandResult

--- src/main/java/com/github/veloproject/userservices/commands/register_new_user/RegisterNewUserCommandResult.java
+++ src/main/java/com/github/veloproject/userservices/commands/register_new_user/RegisterNewUserCommandResult.java
@@ -1,8 +1,10 @@
 package com.github.veloproject.userservices.commands.register_new_user;
 
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 public class RegisterNewUserCommandResult {
-    private Long createdUserId;
+    private Integer createdUserId;
 }
