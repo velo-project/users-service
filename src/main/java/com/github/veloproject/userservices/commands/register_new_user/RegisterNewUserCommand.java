449b4bac5f566e08f32079b5686911a4a239dfd0
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-28 02:49:23 -0300

    feat: make RegisterNewUserCommand implement Request interface

--- src/main/java/com/github/veloproject/userservices/commands/register_new_user/RegisterNewUserCommand.java
+++ src/main/java/com/github/veloproject/userservices/commands/register_new_user/RegisterNewUserCommand.java
@@ -1,11 +1,12 @@
 package com.github.veloproject.userservices.commands.register_new_user;
 
+import com.github.veloproject.userservices.mediators.contracts.Request;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RegisterNewUserCommand {
+public class RegisterNewUserCommand implements Request<RegisterNewUserCommandResult> {
     private String name;
     private String email;
     private String password;
