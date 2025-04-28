e1bf3fa413ea52880e824ade42bfdd15f36375dc
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-28 02:53:10 -0300

    chore: adjust logging messages in Mediator

--- src/main/java/com/github/veloproject/userservices/mediators/LoggingMediatorImp.java
+++ src/main/java/com/github/veloproject/userservices/mediators/LoggingMediatorImp.java
@@ -9,12 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-@Slf4j(topic = "LoggingMediator")
+@Slf4j
 @Component
 public class LoggingMediatorImp implements Mediator {
     private final ApplicationContext appContext;
 
-    @Autowired
     public LoggingMediatorImp(ApplicationContext appContext) {
         this.appContext = appContext;
     }
@@ -27,17 +26,19 @@ public class LoggingMediatorImp implements Mediator {
         var handlerBeanName = Character.toLowerCase(className.charAt(0)) + className.substring(1) + "Handler";
 
         if (!appContext.containsBean(handlerBeanName)) {
-            log.error("No handler bean found: {}", handlerBeanName);
-            throw new NoSuchBeanDefinitionException("Handler not found: " + handlerBeanName);
+            log.error("No handler bean found: {}", className);
+            throw new NoSuchBeanDefinitionException(handlerBeanName);
         }
         try {
             @SuppressWarnings("unchecked")
             var handler = (RequestHandler<Request<TResponse>, TResponse>) appContext.getBean(handlerBeanName);
+            log.info("Handling request: {}", className);
+            var result = handler.handle(request);
+            log.info("Handled successfully: {}", className);
 
-            log.info("Handling request: {}", handlerBeanName);
-            return handler.handle(request);
+            return result;
         } catch (Exception e) {
-            log.error("Bean type invalid: {}", handlerBeanName);
+            log.error("Bean type invalid: {}", className);
             throw new IllegalArgumentException("Bean type invalid: " + handlerBeanName + " |", e);
         }
     }
