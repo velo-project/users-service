d8a5de0ecff3f4358a728d6f0b50a1d2add2f1b7
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-05-02 20:27:40 -0300

    refactor(command): implement RegisterNewUserCommandResult extending Response

--- src/main/java/com/github/veloproject/userservices/commands/register_new_user/RegisterNewUserCommandResult.java
+++ src/main/java/com/github/veloproject/userservices/commands/register_new_user/RegisterNewUserCommandResult.java
@@ -1,10 +1,16 @@
 package com.github.veloproject.userservices.commands.register_new_user;
 
-import lombok.Builder;
+import com.github.veloproject.userservices.mediators.contracts.Response;
 import lombok.Getter;
+import lombok.Setter;
 
-@Builder
 @Getter
-public class RegisterNewUserCommandResult {
+@Setter
+public class RegisterNewUserCommandResult extends Response {
     private Integer createdUserId;
+
+    public RegisterNewUserCommandResult(Integer status, String message, Integer createdUserId) {
+        super(status, message);
+        this.createdUserId = createdUserId;
+    }
 }
