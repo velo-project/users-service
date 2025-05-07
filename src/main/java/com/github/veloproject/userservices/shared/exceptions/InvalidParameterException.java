71f3235b4406aa3db78c73ae763d91466bd6ba62
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-05-07 15:04:33 -0300

    refactor: remove \ from invalid parameter exception message

--- src/main/java/com/github/veloproject/userservices/shared/exceptions/InvalidParameterException.java
+++ src/main/java/com/github/veloproject/userservices/shared/exceptions/InvalidParameterException.java
@@ -2,6 +2,6 @@ package com.github.veloproject.userservices.shared.exceptions;
 
 public class InvalidParameterException extends RuntimeException {
     public InvalidParameterException(String parameter) {
-        super("Error while handling request: Parameter \"" + parameter + "\" invalid.");
+        super("Error while handling request: Parameter '" + parameter + "' invalid.");
     }
 }
