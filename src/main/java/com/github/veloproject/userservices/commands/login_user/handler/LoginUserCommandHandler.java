26b05ce0e847a57ce5cf0feca7c015f6f0cddaf4
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-05-07 00:43:39 -0300

    feat: command handler for login user controller

---
+++ src/main/java/com/github/veloproject/userservices/commands/login_user/handler/LoginUserCommandHandler.java
@@ -0,0 +1,52 @@
+package com.github.veloproject.userservices.commands.login_user.handler;
+
+import com.github.veloproject.userservices.commands.login_user.LoginUserCommand;
+import com.github.veloproject.userservices.commands.login_user.LoginUserCommandResult;
+import com.github.veloproject.userservices.mediators.contracts.RequestHandler;
+import com.github.veloproject.userservices.persistence.repositories.UserRepository;
+import com.github.veloproject.userservices.shared.exceptions.IncorrectInformationsProvided;
+import com.github.veloproject.userservices.shared.utils.CryptographyUtils;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class LoginUserCommandHandler implements RequestHandler<LoginUserCommand, LoginUserCommandResult> {
+    private final JwtEncoder jwtEncoder;
+    private final UserRepository repository;
+
+    public LoginUserCommandHandler(UserRepository repository, JwtEncoder jwtEncoder) {
+        this.repository = repository;
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    @Override
+    public LoginUserCommandResult handle(LoginUserCommand request) {
+        var user = repository.getByEmail(request.getEmail());
+
+        if (user.isEmpty() || !CryptographyUtils.compare(request.getPassword(), user.get().getPassword())) {
+            throw new IncorrectInformationsProvided();
+        }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+        var claims = JwtClaimsSet.builder()
+                .issuer("velo-user-services")
+                .subject(user.get().getId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return new LoginUserCommandResult(
+                200,
+                "Successfully registered.",
+                jwtValue,
+                expiresIn
+        );
+    }
+}
