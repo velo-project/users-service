68f7c121f137b802446fb154156eaa779e84d14f
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-05-11 18:38:19 -0300

    feat: user profile updatable fields enumeration

---
+++ src/main/java/com/github/veloproject/userservices/shared/enums/UserProfileUpdatableFieldsEnum.java
@@ -0,0 +1,11 @@
+package com.github.veloproject.userservices.shared.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserProfileUpdatableFieldsEnum {
+    PREFERRED_NAME,
+    BANNER_PHOTO,
+    PROFILE_PHOTO,
+    DESCRIPTION;
+}
