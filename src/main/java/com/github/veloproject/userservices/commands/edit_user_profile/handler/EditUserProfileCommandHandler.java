6af62ab9fadf7596f7fdf3490cc67b9b7a693324
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-05-11 19:10:43 -0300

    separate edit user profile stages into methods

--- src/main/java/com/github/veloproject/userservices/commands/edit_user_profile/handler/EditUserProfileCommandHandler.java
+++ src/main/java/com/github/veloproject/userservices/commands/edit_user_profile/handler/EditUserProfileCommandHandler.java
@@ -3,11 +3,14 @@ package com.github.veloproject.userservices.commands.edit_user_profile.handler;
 import com.github.veloproject.userservices.commands.edit_user_profile.EditUserProfileCommand;
 import com.github.veloproject.userservices.commands.edit_user_profile.EditUserProfileCommandResult;
 import com.github.veloproject.userservices.mediators.contracts.RequestHandler;
+import com.github.veloproject.userservices.persistence.entities.UserEntity;
 import com.github.veloproject.userservices.persistence.repositories.UserRepository;
+import com.github.veloproject.userservices.shared.enums.UserProfileUpdatableFieldsEnum;
 import com.github.veloproject.userservices.shared.exceptions.InvalidParameterException;
 import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EditUserProfileCommandHandler implements RequestHandler<EditUserProfileCommand, EditUserProfileCommandResult> {
@@ -18,23 +21,19 @@ public class EditUserProfileCommandHandler implements RequestHandler<EditUserPro
     }
 
     @Override
+    @Transactional
     public EditUserProfileCommandResult handle(EditUserProfileCommand request,
                                                JwtAuthenticationToken token) {
-        if (token == null) throw new InvalidBearerTokenException("Bearer token is null.");
-        var user = repository.getReferenceById(Integer.valueOf(token.getName()));
+        if (request.getField() == null) throw new InvalidParameterException("Field must be specified.");
+        else if (request.getFieldValue() == null) throw new InvalidParameterException("fieldValue must be specified.");
+        else if (token == null) throw new InvalidBearerTokenException("Bearer Token must be specified.");
 
-        switch (request.getField()) {
-            case BANNER_PHOTO -> user.setBannerPhotoUrl(request.getFieldValue());
-            case DESCRIPTION -> user.setDescription(request.getFieldValue());
-            case PROFILE_PHOTO -> user.setProfilePhotoUrl(request.getFieldValue());
-            case PREFERRED_NAME -> user.setPrefferedName(request.getFieldValue());
-            default -> throw new InvalidParameterException("field");
-        }
-        repository.save(user);
+        var user = repository.getReferenceById(Integer.valueOf(token.getName()));
+        updateField(request.getField(), request.getFieldValue(), user);
 
         return new EditUserProfileCommandResult(
                 200,
-                "Field '" + request.getField() + "' successfully modified."
+                "Field '" + request.getField() + "' successfully updated."
         );
     }
 
@@ -42,5 +41,24 @@ public class EditUserProfileCommandHandler implements RequestHandler<EditUserPro
     @Deprecated
     public EditUserProfileCommandResult handle(EditUserProfileCommand request) {
         throw new UnsupportedOperationException("Error while handling request: Not supported.");
+    }
+
+    private void updateField(UserProfileUpdatableFieldsEnum field, String fieldValue, UserEntity user) {
+        switch (field) {
+            case BANNER_PHOTO -> user.setBannerPhotoUrl(fieldValue);
+            case DESCRIPTION -> user.setDescription(fieldValue);
+            case PROFILE_PHOTO -> user.setProfilePhotoUrl(fieldValue);
+            case PREFERRED_NAME -> {
+                validatePreferredName(fieldValue);
+                user.setPrefferedName(fieldValue);
+            }
+            default -> throw new InvalidParameterException("Field '" + field + "' is not supported to update.");
+        }
+
+        repository.save(user);
+    }
+
+    private void validatePreferredName(String fieldValue) {
+        if (fieldValue.length() > 20 || fieldValue.length() < 2) throw new InvalidParameterException("Preferred name field value must have between 2 and 20 characters.");
     }
 }
