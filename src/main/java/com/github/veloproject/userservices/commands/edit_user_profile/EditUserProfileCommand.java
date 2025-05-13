3f43a7f7c02d893dd74043a31048cc5b563679c3
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-05-13 15:53:17 -0300

    refactor: rename enum to UserProfileUpdatableField to match class field name

--- src/main/java/com/github/veloproject/userservices/commands/edit_user_profile/EditUserProfileCommand.java
+++ src/main/java/com/github/veloproject/userservices/commands/edit_user_profile/EditUserProfileCommand.java
@@ -1,13 +1,13 @@
 package com.github.veloproject.userservices.commands.edit_user_profile;
 
 import com.github.veloproject.userservices.mediators.contracts.Request;
-import com.github.veloproject.userservices.shared.enums.UserProfileUpdatableFieldsEnum;
+import com.github.veloproject.userservices.shared.enums.UserProfileUpdatableField;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EditUserProfileCommand implements Request<EditUserProfileCommandResult> {
-    private UserProfileUpdatableFieldsEnum field;
+    private UserProfileUpdatableField field;
     private String fieldValue;
 }
