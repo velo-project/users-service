4cca19b5751a5680a025f3162f91420a5b4ad416
jpaveiro <jpaveiros@gmail.com>
2025-05-18 02:26:48 -0300

    VLA-13 | feat: authorize block user route

--- src/main/java/com/github/veloproject/userservices/api/config/SecurityConfig.java
+++ src/main/java/com/github/veloproject/userservices/api/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +24,7 @@ import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Value("${jwt.public.key}")
     private RSAPublicKey publicKey;
@@ -44,8 +46,9 @@ public class SecurityConfig {
                                 "/api/user_services/register")
                         .permitAll()
 
-                        .requestMatchers(HttpMethod.PUT,
-                                "api/user_services/edit_profile")
+                        .requestMatchers(HttpMethod.PATCH,
+                                "/api/user_services/edit_profile",
+                                "/api/user_services/block")
                         .permitAll()
                     .anyRequest().authenticated())
                 .csrf(csrf -> csrf
