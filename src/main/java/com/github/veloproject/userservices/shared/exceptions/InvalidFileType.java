9191a1013a3f221ec922f01ed493dcafe269949b
jpaveiro <jpaveiros@gmail.com>
2025-06-04 01:28:18 -0300

    feat: generic invalid file type exception

---
+++ src/main/java/com/github/veloproject/userservices/shared/exceptions/InvalidFileType.java
@@ -0,0 +1,7 @@
+package com.github.veloproject.userservices.shared.exceptions;
+
+public class InvalidFileType extends RuntimeException {
+    public InvalidFileType(String message) {
+        super(message);
+    }
+}
