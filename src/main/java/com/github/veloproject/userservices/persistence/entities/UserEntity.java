7ad8060f1daf8533d91876443b487bf8376c69df
jpaveiro <jpaveiros@gmail.com>
2025-06-04 14:50:01 -0300

    chore: add user entity properties to make reading more clean

--- src/main/java/com/github/veloproject/userservices/persistence/entities/UserEntity.java
+++ src/main/java/com/github/veloproject/userservices/persistence/entities/UserEntity.java
@@ -16,43 +16,78 @@ import java.util.Set;
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_user")
+    @Column(
+            name = "id_user"
+    )
     private Integer id;
 
-    @Column(name = "name_user")
+    @Column(
+            name = "name_user",
+            length = 100,
+            nullable = false
+    )
     private String name;
 
-    @Column(name = "email_user")
+    @Column(
+            name = "email_user",
+            length = 60,
+            nullable = false,
+            unique = true
+    )
     private String email;
 
     /**
      * @apiNote Por favor, muito cuidado ao tirar a anotação @JsonIgnore deste atributo.
      */
     @JsonIgnore
-    @Column(name = "password_user")
+    @Column(
+            name = "password_user",
+            length = 60,
+            nullable = false
+    )
     private String password;
 
-    @Column(name = "nickname_user")
+    @Column(
+            name = "nickname_user",
+            length = 20,
+            nullable = false,
+            unique = true
+    )
     private String nickname;
 
-    @Column(name = "banner_photo_url_user")
+    @Column(
+            name = "banner_photo_url_user",
+            columnDefinition = "TEXT"
+    )
     private String bannerPhotoUrl;
 
-    @Column(name = "profile_photo_url_user")
+    @Column(
+            name = "profile_photo_url_user",
+            columnDefinition = "TEXT"
+    )
     private String profilePhotoUrl;
 
-    @Column(name = "description_user")
+    @Column(
+            name = "description_user",
+            length = 255
+    )
     private String description;
 
-    @Column(name = "blocked_user")
+    @Column(
+            name = "blocked_user"
+    )
     private Boolean isBlocked;
 
-    @Column(name = "deleted_user")
+    @Column(
+            name = "deleted_user"
+    )
     private Boolean isDeleted;
 
     @Setter(AccessLevel.PRIVATE)
     @CreationTimestamp
-    @Column(name = "registered_at")
+    @Column(
+            name = "registered_at"
+    )
     private LocalDateTime registeredAt;
 
     @ManyToMany(
