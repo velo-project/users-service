a4529f0e555bf5e9b40d24c4cb9584a26dad622e
jpaveiro <jpaveiros@gmail.com>
2025-06-15 17:59:55 -0300

    feat: enable caching on application

--- src/main/java/com/github/veloproject/userservices/UserServicesApplication.java
+++ src/main/java/com/github/veloproject/userservices/UserServicesApplication.java
@@ -2,10 +2,11 @@ package com.github.veloproject.userservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class UserServicesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserServicesApplication.class, args);
     }
