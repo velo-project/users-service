3bf71efdb7d707f51a79167a7a822554d73efbf0
jpaveiro <jpaveiros@gmail.com>
2025-06-17 02:25:36 -0300

    refactor: update image path builder

--- src/main/java/com/github/veloproject/userservices/shared/files/ImageService.java
+++ src/main/java/com/github/veloproject/userservices/shared/files/ImageService.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
 import java.time.LocalDateTime;
 
 @Service
@@ -16,15 +17,20 @@ public class ImageService {
     @Value("${app.image.upload-dir}")
     private String uploadDir;
 
+    // TODO Criar método utilizando o UserUpdatableFields.
     public String uploadImage(MultipartFile file,
                               String fileFinalName,
                               Integer userId)
             throws IOException, InvalidFileType, InvalidParameterException {
         if (fileFinalName == null || fileFinalName.isEmpty()) {
-            throw new  InvalidParameterException("File final name must be specified.");
+            throw new InvalidParameterException("Final name must be specified.");
+        }
+        else if (userId == null) {
+            throw new InvalidParameterException("User id must be specified.");
         }
 
-        var fileName = file.getOriginalFilename();
+        var fileName = file
+                .getOriginalFilename();
         if (fileName == null || fileName.isEmpty()) {
             throw new InvalidParameterException("File name must be not null.");
         }
@@ -35,22 +41,30 @@ public class ImageService {
         }
 
         var now = LocalDateTime.now();
-        String filePath = "/"
-                + now.getYear() + "/"
-                + now.getMonthValue() + "/"
-                + now.getDayOfMonth()
-                + "/users"
-                + "/" + userId
-                + "/" + userId + "_" + fileFinalName;
 
-        String finalPath = Paths.get(uploadDir, filePath).toString();
+        var year = String.valueOf(now.getYear());
+        var month = now.getMonthValue();
+        var day = now.getDayOfMonth();
+        var userIdToString = String.valueOf(userId);
+
+        String filePath = MessageFormat
+                .format(
+                        "/{0}/{1}/{2}/users/{3}/{3}_{4}",
+                        year,
+                        month,
+                        day,
+                        userIdToString,
+                        fileFinalName
+                );
+        String finalPath = Paths
+                .get(uploadDir, filePath)
+                .toString();
+
         File finalFile = new File(finalPath);
-
         File parentDir = finalFile.getParentFile();
         if (!parentDir.exists()) {
             parentDir.mkdirs();
         }
-
         file.transferTo(finalFile);
 
         return finalPath;
