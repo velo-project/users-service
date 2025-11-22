315e29ac5d22981198c952c1425a95751549b938
jpaveiro <jpaveiros@gmail.com>
2025-11-22 04:10:03 -0300

    fix(exception): handler handling wrong exception class

--- presentations/src/main/java/com/github/veloproject/userservices/presentations/advicers/InvalidParameterExceptionHandler.java
+++ presentations/src/main/java/com/github/veloproject/userservices/presentations/advicers/InvalidParameterExceptionHandler.java
@@ -1,11 +1,10 @@
 package com.github.veloproject.userservices.presentations.advicers;
 
 import com.github.veloproject.userservices.application.mediators.contracts.Response;
+import com.github.veloproject.userservices.domain.exceptions.InvalidParameterException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.security.InvalidParameterException;
 
 @RestControllerAdvice
 public class InvalidParameterExceptionHandler {
