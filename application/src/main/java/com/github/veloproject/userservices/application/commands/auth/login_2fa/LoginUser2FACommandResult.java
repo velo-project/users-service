79d0a934d035df597a96c246d2e695c0b88cb8f6
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-11-27 20:01:52 -0300

    refactor: remove refreshTokenExpiresIn

--- application/src/main/java/com/github/veloproject/userservices/application/commands/auth/login_2fa/LoginUser2FACommandResult.java
+++ application/src/main/java/com/github/veloproject/userservices/application/commands/auth/login_2fa/LoginUser2FACommandResult.java
@@ -9,14 +9,10 @@ import lombok.Setter;
 public class LoginUser2FACommandResult extends Response {
     private String accessToken;
     private Long expiresIn;
-    private String refreshToken;
-    private Long refreshTokenExpiresIn;
 
-    public LoginUser2FACommandResult(Integer statusCode, String message, String accessToken, Long expiresIn, String refreshToken, Long refreshTokenExpiresIn) {
+    public LoginUser2FACommandResult(Integer statusCode, String message, String accessToken, Long expiresIn) {
         super(statusCode, message);
         this.accessToken = accessToken;
         this.expiresIn = expiresIn;
-        this.refreshToken = refreshToken;
-        this.refreshTokenExpiresIn = refreshTokenExpiresIn;
     }
 }
